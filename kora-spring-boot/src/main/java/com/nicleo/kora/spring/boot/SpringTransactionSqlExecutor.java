75244988c582f78364442ee4248823034dc13d23
aabb <abab@a.com>
2026-04-12 16:55:38 +0900

    sqlsession->sqlexecutor

---
+++ kora-spring-boot/src/main/java/com/nicleo/kora/spring/boot/SpringTransactionSqlExecutor.java
@@ -0,0 +1,24 @@
+package com.nicleo.kora.spring.boot;
+
+import com.nicleo.kora.core.runtime.SqlInterceptor;
+import com.nicleo.kora.core.runtime.jdbc.DefaultSqlExecutor;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.util.Collection;
+
+public class SpringTransactionSqlExecutor extends DefaultSqlExecutor {
+    public SpringTransactionSqlExecutor(DataSource dataSource) {
+        super(dataSource);
+    }
+
+    public void addInterceptor(Collection<SqlInterceptor> interceptors) {
+        super.addInterceptor(interceptors);
+    }
+
+    @Override
+    protected Connection openConnection() {
+        return new DelegateConnection(DataSourceUtils.getConnection(getDataSource()));
+    }
+}
